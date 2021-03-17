42581c692a27ed372a75e12f3dd8fff00cf20cdb
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-17 07:23:34 +0600

    implement simple google authentication system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
+
+    const [user, setUser] = useState({})
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log(token, user);
+                setUser(user);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log(errorCode, errorMessage, email, credential);
+            });
+    }
+
     return (
         <div className="App">
-
+            <button onClick={handleGoogleSignIn}>sign in using google</button>
+            <h2>{user.displayName}</h2>
+            <h3>{user.email}</h3>
+            <img src={user.photoURL} alt="" />
         </div>
     );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDyxQo5y9QzaT6PlI9deXb9lrJZyx53cA",
+    authDomain: "authentication-firebase-recap.firebaseapp.com",
+    projectId: "authentication-firebase-recap",
+    storageBucket: "authentication-firebase-recap.appspot.com",
+    messagingSenderId: "923669757199",
+    appId: "1:923669757199:web:57ed78de1e83fb3251d27a"
+};
+
+export default firebaseConfig;
