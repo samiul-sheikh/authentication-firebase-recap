f6b204471723e8444c79a16acbc3dbe79345f070
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-18 08:09:29 +0600

    implement github authentication system

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
     var provider = new firebase.auth.GoogleAuthProvider();
     var fbProvider = new firebase.auth.FacebookAuthProvider();
+    var ghProvider = new firebase.auth.GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
         firebase.auth()
@@ -54,11 +55,33 @@ function App() {
             });
     }
 
+    const handleGithubSignIn = () => {
+        firebase
+            .auth()
+            .signInWithPopup(ghProvider)
+            .then((result) => {
+                var credential = result.credential;
+                var token = credential.accessToken;
+                var user = result.user;
+                console.log('gh user', user)
+                setUser(user);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+                console.log('error', errorCode, errorMessage, email, credential);
+            });
+    }
+
     return (
         <div className="App">
             <button onClick={handleGoogleSignIn}>sign in using google</button>
             <br />
             <button onClick={handleFacebookSignIn}>sign in using facebook</button>
+            <br />
+            <button onClick={handleGithubSignIn}>sign in using github</button>
             <h2>{user.displayName}</h2>
             <h3>{user.email}</h3>
             <img src={user.photoURL} alt="" />
